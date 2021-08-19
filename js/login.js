113eb2aa6b4ad6721b3e0ab0cfe0a1f384d6eaee
rodrigocanto1 <rocap76@gmail.com>
2021-08-19 17:02:08 -0300

    entregable 1

--- js/login.js
+++ js/login.js
@@ -2,5 +2,39 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+});
+function verificar(){
+    let user = document.getElementById("user");
+    let pass = document.getElementById("pass");
+    let msj = document.getElementById ("msj");
+    let usuario = {};
+   
+    if ( user.value.trim() ==='' || pass.value.trim()==='' ){
+
+        user.classList.add("isInvalid"); 
+        msj.innerHTML="Dato requerido";
+        msj.classList.add("isInvalid");
+        msj.style.color="red";
+        msj.style.display="block";            
+    
+    }
+    else{
+        
+        location.href="index.html";
+
+        usuario.nombre = user.value;
+        usuario.estado = "Conectado";
+
+        localStorage.setItem('usuario',JSON.stringify(usuario));
+        sessionStorage.setItem('usuario',JSON.stringify(usuario));
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    let usuario = JSON.parse( localStorage.getItem("usuario"));
+    if (usuario.estado=='conectado'){
+        location.href="pagina.html";
+    }
 
 });
